9f0444fbe67e910800395d32963f00959f62cbc5
JoseCardona117 <josecardona117@outlook.com>
2023-05-26 15:38:03 -0600

    Adding a register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  model: any = {};
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  register(): void {
+    console.log(this.model);
+  }
+
+  cancel(): void {
+    console.log("Cancelled")
+  }
 }
